630fc5fd1577e709fd201f1e50f2b6e6d298352a
Vinicius Morais <vinicius17@gmail.com>
2025-10-29 08:47:30 -0500

    Initial Commit

---
+++ Cooking App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cooking App
+//
+//  Created by Vinicius Morais on 10/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cooking App/Cooking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cooking_AppApp.swift
+//  Cooking App
+//
+//  Created by Vinicius Morais on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Cooking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
